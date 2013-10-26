d51d82ccb9d857ee2a8e5bd27a074327140046c5
Max <maksr51314@gmail.com>
2013-10-26 15:14:44 +0300

    update keyMap

--- app/js/entities/tanks/current-tank.js
+++ app/js/entities/tanks/current-tank.js
@@ -17,6 +17,35 @@ angular.module('app').factory('CurrentTank', function(Entity) {
     }
 
     CurrentTank.prototype.tick = function() {
+        CurrentTank.prototype.updateSpeed();
+    };
+
+    CurrentTank.prototype.updateSpeed = function() {
+
+        switch (this.keyMap.left +'|'+ this.keyMap.up +'|'+ this.keyMap.right +'|'+ this.keyMap.down) {
+            case false +'|'+ false +'|'+ false +'|'+ false:
+                this.entity.speed.x = 0;
+                this.entity.speed.y = 0;
+                break;
+            case true +'|'+ false +'|'+ false +'|'+ false:
+                this.entity.speed.x = -1;
+                this.entity.speed.y = 0;
+                break;
+            case false +'|'+ true +'|'+ false +'|'+ false:
+                this.entity.speed.x = 0;
+                this.entity.speed.y = 1;
+                break;
+            case false +'|'+ false +'|'+ true +'|'+ false:
+                this.entity.speed.x = 1;
+                this.entity.speed.y = 0;
+                break;
+            case false +'|'+ false +'|'+ false +'|'+ true:
+                this.entity.speed.x = 0;
+                this.entity.speed.y = -1;
+                break;
+
+
+        }
 
     };
 
