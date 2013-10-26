77a1e661d685a6eb021c8cb5873dd383ea542885
Andrey Nikitiuk <demonshi.ua@gmail.com>
2013-10-26 12:12:11 +0300

    implement point.js

--- app/js/core/point.js
+++ app/js/core/point.js
@@ -1,3 +1,12 @@
 /**
  * Created by city on 10/26/13.
  */
+
+angular.module('app').factory('Point', function() {
+    function Point(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    return Point;
+})
